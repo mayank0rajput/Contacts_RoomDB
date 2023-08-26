8595d3ae2fb273d08bdd6234cc9591e101c9fc93
mayank <mayankrajput40558@gmail.com>
2023-08-26 18:17:03 +0530

    fix: alert dialog box dismiss request issue

--- app/src/main/java/com/example/roomdb/AddContactDialog.kt
+++ app/src/main/java/com/example/roomdb/AddContactDialog.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.DialogProperties
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -27,6 +28,9 @@ fun AddContactDialog(
         onDismissRequest = {
             onEvent(ContactEvent.HideDialog)
         },
+        properties = DialogProperties(
+            dismissOnClickOutside = false
+        ),
         title = { Text(text = "Add contact") },
         text = {
             Column(
